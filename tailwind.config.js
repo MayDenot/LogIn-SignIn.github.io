c0fb41391c727bf87c86416b17d9013872f2d369
May <mayra.denot@gmail.com>
2023-08-22 17:52:26 -0300

    Arreglo de la ruta del BG

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export const content = ['./dist/**/*.{js}', 'index.html']
 export const theme = {
   extend: {
     backgroundImage: {
-      'bg-image': "url('./dist/background.jpg')"
+      image: "url('../dist/background.jpg')"
     },
     fontFamily: {
       poppins: ['Poppins', 'Cascadia Mono PL', 'Cascadia Mono']
